0f024920e2bd9b8cd6a157ca54a4f14ba5e9f523
Alexandr-Lys <a.lysenko00@gmail.com>
2020-06-15 16:12:17 +0400

    work

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import classes from './MyPosts.module.css';
+import Post from './Post/Post.jsx';
 const MyPosts = () => {
-return  <div className={classes.posts}>My posts<div>New post</div>
-        <div className={classes.item}>post1</div>
-        <div className={classes.item}>post2</div>
+return  <div>My posts
+            <div>
+                <textarea></textarea>
+                <button>ADD POST</button>
+            </div>
+            <div className={classes.posts}>
+                <Post message='Hi, how are you?'/>
+                <Post message="It's my first post"/>
+            </div>
         </div>
 
 }
-
 export default MyPosts;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './Profile.module.css';
-import MyPosts from './MyPosts/MyPosts.jsx'
+import MyPosts from './MyPosts/MyPosts.jsx';
 
 const Profile = () => {
 return  <div className={classes.content}>
